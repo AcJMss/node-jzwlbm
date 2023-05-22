0c3e9e5e65d6da60af0bdb3d7d5f4c5aa006b61e
AcJMss <848474913@qq.com>
2023-05-22 18:18:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-jzwlbm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-jzwlbm)
---
+++ index.js
@@ -0,0 +1,7 @@
+/** Some other codes maybe */
+const handleData = (/** Some params maybe */) => {
+  // Your code here
+};
+
+const data = handleData();
+console.log(data);
